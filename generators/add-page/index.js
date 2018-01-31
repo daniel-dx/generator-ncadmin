3f885896e6f47661cbfc7946f8eed382cd751537
kyle.lo <kyle.lo@vipshop.com>
2018-01-31 17:38:56 +0800

    add-page

--- generators/add-page/index.js
+++ generators/add-page/index.js
@@ -5,77 +5,121 @@ var glob = require("glob");
 var path = require('path');
 var s = require('underscore.string');
 var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
 
 var logger = require('../app/logger');
 var utils = require('../app/utils');
+var fs = require('fs');
 
 var scrFolderPath, scrFolder;
 
 module.exports = yeoman.Base.extend({
 
   prompting() {
+    const moduleList = fs.readdirSync('src/modules').filter(function (item) {
+      return item.indexOf('.') == -1;
+    }).map(function (item) {
+      return {
+        name: item,
+        value: item
+      }
+    })
 
-    // var prompts = [{
-    //   type: 'string',
-    //   name: 'name',
-    //   message: 'Would you like to enable this option?',
-    //   default: 'name'
-    // }];
+    // 询问信息
+    var prompts = [
+      {
+        type: 'list',
+        name: 'moduleName',
+        message: 'Chose the module.',
+        choices: moduleList,
+        default: moduleList[0].value
+      },
+      {
+        type: 'string',
+        name: 'name',
+        message: 'Input the page name.(Use \'-\' to connect words.)',
+        default: `page-${new Date().getTime()}`
+      }
+    ];
 
-    // return this.prompt(prompts).then(props => {
-    //   // To access props later use this.props.someAnswer;
-    //   this.props = props;
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+      // 中划线name
+      this.props.midLineName = s(this.props.name).underscored().slugify().value();
+      // 驼峰name
+      this.props.camelName = s(this.props.midLineName).camelize().value();
+      // 首字母大写驼峰
+      this.props.firstCapCamelComponentName = s(this.props.camelName).capitalize().value(); // => DemoUser
+      // 模块路径
+      this.props.modulePath = `./src/modules/${this.props.moduleName}`;
+    });
 
-    //   // example: name = demo-user
-    //   this.props.componentName = s(this.props.name).underscored().slugify().value(); // => demo-user
-    //   this.props.camelComponentName = s(this.props.componentName).camelize().value(); // => demoUser
-    //   this.props.firstCapCamelComponentName = s(this.props.camelComponentName).capitalize().value(); // => DemoUser
+  },
 
-    //   scrFolder = 'src/components/' + this.props.componentName;
-    //   scrFolderPath = './' + scrFolder + '/';
-
-    // });
-
+  checkPage() {
+    this.props.pagePath = path.join(this.props.modulePath, 'pages', this.props.midLineName + '.vue');
+    if (fs.existsSync(this.props.pagePath)) {
+      throw new Error(`Page:${this.props.pagePath} is already exist.`);
+    }
   },
 
   copyTemplates() {
-
-    // var done = this.async();
-
-    // glob(this.templatePath() + "/**/*.*", {}, (er, files) => {
-    //   _.each(files, filePath => {
-    //     var toFileName = path.parse(filePath).base;
-    //     this.fs.copyTpl(
-    //       filePath,
-    //       path.resolve(scrFolderPath, toFileName),
-    //       this.props
-    //     );
-    //   });
-
-    //   done();
-    // });
-
+    return this.fs.copyTpl(
+      this.templatePath('page.vue'),
+      this.destinationPath(this.props.pagePath),
+      {
+        midLineName: this.props.midLineName
+      }
+    );
   },
 
-  updateContent() {
+  updateNav() {
+    var fullPath = './src/common/components/layout-nav.vue';
 
-    // var fullPath = 'src/components/App.vue';
-    // utils.rewriteFile({
-    //   fileRelativePath      : fullPath,
-    //   insertPrev: true,
-    //   needle    : "<!-- Don't touch me -->",
-    //   splicable : [
-    //     `<${this.componentName}></${this.componentName}>`
-    //   ]
-    // });
+    utils.rewriteFile({
+      fileRelativePath: fullPath,
+      insertPrev: true,
+      needle: `<!-- Don't touch me - ${this.props.moduleName} pages -->`,
+      splicable: [
+        `{`,
+        `  icon: "",`,
+        `  name: "${this.props.firstCapCamelComponentName}",`,
+        `  link: "/${this.props.moduleName}/${this.props.midLineName}"`,
+        `},`,
+      ]
+    });
+  },
 
+  updateRoute() {
+    var fullPath = './src/routes/index.js';
+
+    const midLineModulePage = `${this.props.moduleName}-${this.props.midLineName}`;
+    const camelModulePage = s(midLineModulePage).camelize().value();
+
+    utils.rewriteFile({
+      fileRelativePath: fullPath,
+      insertPrev: true,
+      needle: `<!-- Don't touch me - import ${this.props.moduleName} -->`,
+      splicable: [
+        `import ${camelModulePage} from 'modules/${this.props.moduleName}/pages/${this.props.midLineName}.vue';`,
+      ]
+    });
+    utils.rewriteFile({
+      fileRelativePath: fullPath,
+      insertPrev: true,
+      needle: `<!-- Don't touch me - export ${this.props.moduleName} -->`,
+      splicable: [
+        `{ path: '/${this.props.moduleName}/${this.props.midLineName}', component: ${camelModulePage}, name: '${midLineModulePage}' },`,
+      ]
+    });
   },
 
   usageTip() {
-    logger.log('=========================');
-    logger.log('Congratulations, completed successfully!');
-    logger.log("Gook Luck!");
-    logger.log('=========================');
+    logger.green('=========================');
+    logger.green('Congratulations, completed successfully!');
+    logger.green('=========================');
+    logger.log(`   ${chalk.yellow('modify')} src/routes/index.js`);
+    logger.log(`   ${chalk.yellow('modify')} src/common/components/layout-nav.vue`);
   }
 
 });
---
+++ generators/add-page/templates/page.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="<%= midLineName %>">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+  created() {
+      
+  },
+  data() {
+    return {
+      msg: "hello world",
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" rel="stylesheet/scss" scoped>
+.<%= midLineName %>{
+
+}
+</style>
