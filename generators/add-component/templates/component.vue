975387d3f06c69fed6b10232b18eeceb2cbf8cfc
kyle.lo <kyle.lo@vipshop.com>
2018-01-31 21:44:20 +0800

    fixed

--- generators/add-component/templates/component.vue
+++ generators/add-component/templates/component.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="<%= midLineName %>">
-
+    <%= midLineName %>
   </div>
 </template>
 
--- generators/add-page/templates/page.vue
+++ generators/add-page/templates/page.vue
@@ -11,7 +11,7 @@ export default {
   },
   data() {
     return {
-      msg: "hello world",
+      msg: "<%= midLineName %>",
     }
   }
 };
